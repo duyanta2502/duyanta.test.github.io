11a71576c2a35ac54ccca2f96b2659a35532bc7d
An <tda25022003@gmail.com>
2025-03-04 14:14:49 +0700

    Fix app.js and pakage.json  for Railway deployment

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 var express = require("express");
 var app = express();
 var controller = require(__dirname +"/apps/controllers");
+
+const PORT = process.env.PORT || 3000; 
+
 app.use(controller);
-var server = app.listen(3000, function(){
-console.log("server is running");});
+
 
 app.set("views",__dirname + "/apps/views");
 app.set("view engine","ejs");
 app.use("/static", express.static(__dirname + "/public"));
 app.use("/partical", express.static(__dirname + "/views/partical"));
+
+// Khởi động server
+app.listen(PORT, function () {
+    console.log(`🚀 Server is running on port ${PORT}`);
+});
